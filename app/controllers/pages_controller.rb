a2f61f6f84a8b6b862bb395539f136b628cc9f9f
Arun Srinivasan <arunrocks@gmail.com>
2011-02-04 18:31:16 -0600

    ruby-chap4

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,19 @@
 class PagesController < ApplicationController
-  @@head_title = "RoR tutorial"
+  
   def home
     @title ='Home'
-    @full_title= @@head_title + " | " + @title
+    
   end
 
   def about
     @title = "About"
-    @full_title= @@head_title + " | " + @title
+    
     
   end
   
   def contact
     @title = "Contact"
-    @full_title= @@head_title + " | " + @title
+    
     
   end
   
