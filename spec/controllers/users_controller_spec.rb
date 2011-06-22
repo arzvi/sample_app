f37143eeeb72e3aa869ffaa79bf6f5914c34fa3a
arzvi <arunrocks@gmail.com>
2011-06-22 15:20:02 -0500

    half 8

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -48,5 +48,50 @@ describe UsersController do
     end
     
   end
-
+  
+  describe "POST 'create'" do 
+    describe "failure" do
+      
+      before(:each) do 
+        @attr = { :name => "", :email => "", :password => "", :password_confirmation => ""}
+      end
+      
+      it "should not create a user" do 
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+      
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Sign up")
+      end
+      
+      it "should render the 'new' page" do 
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+    
+    describe "success" do 
+      before(:each) do
+        @attr= { :name => "Arun Srini", :email => "arunrocks@gmail.com", :password=>"foo",
+          :password_confirmation => "foo"}
+      end
+      
+      it "should create an user" do
+        lambda do 
+          post :create, :user => @attr
+        end.should change(:user.count).by(1)
+      end
+      
+      it "should redirect to user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+      
+      
+    end
+  end
 end
+
